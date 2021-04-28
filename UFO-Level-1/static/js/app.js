2c17d970387b908911e92e2ef0a4324ff23ad508
Adolfo <adolfoprieto37@yahoo.com>
2021-04-28 11:56:56 -0500

    starting code

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,4 +1,23 @@
 // from data.js
 var tableData = data;
 
+// View table 
+console.log(tableData);
+
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+
+// UFO Sighting values for each column
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    var row = tbody.append("tr");
+
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+        console.log(key, value);
+        // Append a cell to the row for each value
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
+
